ad76e280cbc1b4baf4985814a3bd9278100e013b
lisx <li@lideMacBook-Pro.local>
2026-02-05 16:32:43 +0800

    first code commmit

---
+++ spring-mcp-final/src/main/java/com/li/spring_mcp_final/SpringMcpFinalApplication.java
@@ -0,0 +1,13 @@
+package com.li.spring_mcp_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcpFinalApplication.class, args);
+	}
+
+}
---
+++ spring-mcp-final/src/test/java/com/li/spring_mcp_final/SpringMcpFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.spring_mcp_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
